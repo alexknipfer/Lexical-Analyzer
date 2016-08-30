d851191d35c04f04e77278cc8e8ba9462453a374
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-08-30 16:10:13 -0500

    initiates tokens

--- LexicalAnalyzer.h
+++ LexicalAnalyzer.h
@@ -1,11 +1,16 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class LexicalAnalyzer{
   public:
     void getFileName(ifstream &);
+    void initiateValidTokens();
+    void scanFile(ifstream &);
 
   private:
+    vector<string> tokenNames;
+    vector<string> tokenDescriptions;
 };
--- lex.cpp
+++ lex.cpp
@@ -9,8 +9,11 @@ using namespace std;
 int main(){
   LexicalAnalyzer lexicalAnalyzer;
   ifstream myFile;
+  string token;
 
   lexicalAnalyzer.getFileName(myFile);
+  //lexicalAnalyzer.initiateValidTokens();
+  lexicalAnalyzer.scanFile(myFile);
 
   return 0;
 }
@@ -30,5 +33,46 @@ void LexicalAnalyzer::getFileName(ifstream &myFile){
     else{
       break;
     }
+  }
+}
+
+void LexicalAnalyzer::scanFile(ifstream &myFile){
+  string readLine;
+  getline(myFile, readLine);
+}
+
+void LexicalAnalyzer::initiateValidTokens(){
+  tokenNames.push_back("ADDOP");
+  tokenNames.push_back("ADDOP");
+  tokenNames.push_back("AND");
+  tokenNames.push_back("ASSIGNOP");
+  tokenNames.push_back("COMMA");
+  tokenNames.push_back("CURLL");
+  tokenNames.push_back("CURLR");
+  tokenNames.push_back("ELSE");
+  tokenNames.push_back("FUNCTION");
+  tokenNames.push_back("ID");
+  tokenNames.push_back("IF");
+  tokenNames.push_back("MULOP");
+  tokenNames.push_back("MULOP");
+  tokenNames.push_back("NOT");
+  tokenNames.push_back("NUMBER");
+  tokenNames.push_back("OR");
+  tokenNames.push_back("PARENL");
+  tokenNames.push_back("PARENR");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RELOP");
+  tokenNames.push_back("RETURN");
+  tokenNames.push_back("SEMICOLON");
+  tokenNames.push_back("STRING");
+  tokenNames.push_back("VAR");
+  tokenNames.push_back("WHILE");
+
+  for(int x = 0; x < tokenNames.size(); x++){
+    cout << tokenNames[x] << endl;
   }
 }
